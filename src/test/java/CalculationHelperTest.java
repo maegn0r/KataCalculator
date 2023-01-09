71a3d2b92c7fdfc78936f7e6e850bf52c14fbbe8
novitskynick <novitskynick@gmail.com>
2023-01-09 03:45:49 +0300

    some tests

---
+++ src/test/java/CalculationHelperTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+class CalculationHelperTest {
+
+
+@Test
+void calcTest(){
+    Assertions.assertEquals("7", CalculationHelper.parseAndCalc("2+5"));
+    Assertions.assertEquals("C", CalculationHelper.parseAndCalc("X*X"));
+    Assertions.assertEquals("-3", CalculationHelper.parseAndCalc("2-5"));
+    Assertions.assertThrows(RuntimeException.class, () -> CalculationHelper.parseAndCalc("V-5"));
+}
+
+
+}
